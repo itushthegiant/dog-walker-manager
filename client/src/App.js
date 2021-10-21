4799e9bf4d2fa2b3884dbb731478d245709bfb8e
etamar <etamar.levy@gmail.com>
2021-10-21 17:50:56 -0400

    login form created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navibar from './components/Navibar'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navibar />
+        <Switch>
+          <Route exact path='/' component={Login}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
